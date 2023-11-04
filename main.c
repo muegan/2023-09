d19b0084d44dda01eadf33f77bc5968e398dd1b3
muegan <2216869@sookmyung.ac.kr>
2023-11-04 13:01:53 +0900

    3번

--- main.c
+++ main.c
@@ -1,27 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define N_STUDENT 5
+#define SIZE 5
 
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
 	
-	int grade[N_STUDENT];
-	int i, average;
-	printf("input 5 scores:");
-	 
-	int sum=0;
-	for(i=0; i<N_STUDENT; i++)
-	{
-		scanf("%d", &grade[i]);
-		sum+=grade[i];
-	}
+	int i;
+	int grade[SIZE];
+	int score[SIZE];
 	
-	average=sum/N_STUDENT;
-	
-	printf("score average: %i\n", average);
+	for (i=0; i<SIZE; i++)
+		grade[i]=rand()%100 + 1;
+		
+	for (i=0; i<SIZE; i++)
+	score[i]=grade[i];
+		
+	for(i=0; i<SIZE; i++)
+		printf("score[%d]=%d (grade:%i)\n", i, score[i], grade[i]);
 	 
 	return 0;
 }
